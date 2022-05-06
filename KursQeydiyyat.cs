d3113b9a381ed6ef3e9f0d1da866811fba010b51
hajizada999 <6hacizade@gmail.com>
2022-05-07 00:10:43 +0400

    First version of Course Registration

---
+++ KursQeydiyyat.cs
@@ -0,0 +1,22 @@
+﻿START:
+Console.WriteLine("Yasinizi qeyd ediniz: ");
+int userAge = Convert.ToInt32(Console.ReadLine());
+if(userAge > 18)
+{
+    Console.WriteLine("Imtahan kecid balinizi qeyd ediniz: ");
+    int userAvgPoint = Convert.ToInt32(Console.ReadLine());
+    if (userAvgPoint > 50)
+    {
+        Console.WriteLine("Sizin kursumuza qeydiyyatiniz muveffeqiyyetle qeyde alinmisdir . Tesekkurler :)");
+    }
+    else
+    {
+        Console.WriteLine("Sizin Baliniz Kursa qeydiyyat ucun yeterli deyildir . ");
+    }
+}
+else
+{
+    Console.WriteLine("Sizin yasiniz kursa qeydiyyat ucun yeterli deyil . ");
+}
+goto START;
+
